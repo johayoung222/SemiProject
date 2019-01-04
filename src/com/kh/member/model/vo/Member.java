0007d037f69028d55e6d3f38018f760b594ea92e
Min_seong <withness77@gmail.com>
2019-01-04 20:03:56 +0900

    vo변경

--- src/com/kh/member/model/vo/Member.java
+++ src/com/kh/member/model/vo/Member.java
@@ -2,91 +2,110 @@ package com.kh.member.model.vo;
 
 public class Member {
 
-	 private String memberid; //회원아이디
-	 private String password; //패스원드
-	 private String name;		//이름
-	 private String gender;		//성별
-	 private int age;			//나이
-	 private String email;		//이메일
-	 private int login_log;		//로그인 누적 카운트
-	 private String status;		//회원상태
-	 private String sns_account;//sns연동
+	 private String memberId; //회원아이디
+	 private String memberPwd; //패스원드
+	 private String memberName;		//이름
+	 private String memberGender;		//성별
+	 private int memberAge;			//나이
+	 private String memberEmail;		//이메일
+	 private int memberLoginlog;		//로그인 누적 카운트
+	 private String memberStatus;		//회원상태
+	 private String memberSnsaccount;//sns연동
 	 
-	 public Member() {}
-	 
-	@Override
+	 @Override
 	public String toString() {
-		return memberid + ", " + password + ", " + name + ", " + gender
-				+ ", " + age + ", " + email + ", " + login_log + ", " + status
-				+ ", " + sns_account;
+		return memberId + ", " + memberPwd + ", " + memberName
+				+ ", " + memberGender + ", " + memberAge + ", " + memberEmail
+				+ ", " + memberLoginlog + ", " + memberStatus + ", "
+				+ memberSnsaccount;
 	}
 
-	public Member(String memberid, String password, String name, String gender, int age, String email, int login_log,
-			String status, String sns_account) {
+	public Member(String memberId, String memberPwd, String memberName, String memberGender, int memberAge,
+			String memberEmail, int memberLoginlog, String memberStatus, String memberSnsaccount) {
 		super();
-		this.memberid = memberid;
-		this.password = password;
-		this.name = name;
-		this.gender = gender;
-		this.age = age;
-		this.email = email;
-		this.login_log = login_log;
-		this.status = status;
-		this.sns_account = sns_account;
+		this.memberId = memberId;
+		this.memberPwd = memberPwd;
+		this.memberName = memberName;
+		this.memberGender = memberGender;
+		this.memberAge = memberAge;
+		this.memberEmail = memberEmail;
+		this.memberLoginlog = memberLoginlog;
+		this.memberStatus = memberStatus;
+		this.memberSnsaccount = memberSnsaccount;
 	}
-	public String getMemberid() {
-		return memberid;
+
+	public String getMemberId() {
+		return memberId;
 	}
-	public void setMemberid(String memberid) {
-		this.memberid = memberid;
+
+	public void setMemberId(String memberId) {
+		this.memberId = memberId;
 	}
-	public String getPassword() {
-		return password;
+
+	public String getMemberPwd() {
+		return memberPwd;
 	}
-	public void setPassword(String password) {
-		this.password = password;
+
+	public void setMemberPwd(String memberPwd) {
+		this.memberPwd = memberPwd;
 	}
-	public String getName() {
-		return name;
+
+	public String getMemberName() {
+		return memberName;
 	}
-	public void setName(String name) {
-		this.name = name;
+
+	public void setMemberName(String memberName) {
+		this.memberName = memberName;
 	}
-	public String getGender() {
-		return gender;
+
+	public String getMemberGender() {
+		return memberGender;
 	}
-	public void setGender(String gender) {
-		this.gender = gender;
+
+	public void setMemberGender(String memberGender) {
+		this.memberGender = memberGender;
 	}
-	public int getAge() {
-		return age;
+
+	public int getMemberAge() {
+		return memberAge;
 	}
-	public void setAge(int age) {
-		this.age = age;
+
+	public void setMemberAge(int memberAge) {
+		this.memberAge = memberAge;
 	}
-	public String getEmail() {
-		return email;
+
+	public String getMemberEmail() {
+		return memberEmail;
 	}
-	public void setEmail(String email) {
-		this.email = email;
+
+	public void setMemberEmail(String memberEmail) {
+		this.memberEmail = memberEmail;
 	}
-	public int getLogin_log() {
-		return login_log;
+
+	public int getMemberLoginlog() {
+		return memberLoginlog;
 	}
-	public void setLogin_log(int login_log) {
-		this.login_log = login_log;
+
+	public void setMemberLoginlog(int memberLoginlog) {
+		this.memberLoginlog = memberLoginlog;
 	}
-	public String getStatus() {
-		return status;
+
+	public String getMemberStatus() {
+		return memberStatus;
 	}
-	public void setStatus(String status) {
-		this.status = status;
+
+	public void setMemberStatus(String memberStatus) {
+		this.memberStatus = memberStatus;
 	}
-	public String getSns_account() {
-		return sns_account;
+
+	public String getMemberSnsaccount() {
+		return memberSnsaccount;
 	}
-	public void setSns_account(String sns_account) {
-		this.sns_account = sns_account;
+
+	public void setMemberSnsaccount(String memberSnsaccount) {
+		this.memberSnsaccount = memberSnsaccount;
 	}
+
+	public Member() {}
 	 
 }
