1606d63ae445c14c22486f089e8f4d826dcba583
ck <ekzmdlsej@naver.com>
2019-01-11 19:12:53 +0900

    scheduleInfo.jsp 다자인 수정 / Schedule update/delete servlet 뼈대

--- src/com/kh/schedule/controller/InsertScheduleEndServlet.java
+++ src/com/kh/schedule/controller/InsertScheduleEndServlet.java
@@ -169,7 +169,7 @@ public class InsertScheduleEndServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	
 		doGet(request, response);
 	}
 
